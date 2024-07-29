f20c2349d6fc6cd12fa52d71ca36d5e6908c80a4
Ankika Dey <ankikadey@outlook.com>
2024-07-29 22:08:30 +0530

    add SingletonPatternExample

---
+++ Week1/Design-Patterns-and-Principles/SingletonPatternExample/Logger.java
@@ -0,0 +1,19 @@
+public class Logger {
+    private static Logger instance;
+
+    private Logger() {
+        // private constructor to prevent instantiation
+    }
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
+
---
+++ Week1/Design-Patterns-and-Principles/SingletonPatternExample/SingletonTest.java
@@ -0,0 +1,10 @@
+public class SingletonTest {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        System.out.println("Are both instances the same? " + (logger1 == logger2));
+
+        logger1.log("This is a test message.");
+    }
+}
