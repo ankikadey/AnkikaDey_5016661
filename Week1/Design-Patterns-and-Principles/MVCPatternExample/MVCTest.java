390dda36c8ad07702682efaa85cf26881c37855a
Ankika Dey <ankikadey@outlook.com>
2024-07-30 11:36:00 +0530

    add implementation class

---
+++ Week1/Design-Patterns-and-Principles/MVCPatternExample/MVCTest.java
@@ -0,0 +1,13 @@
+public class MVCTest {
+    public static void main(String[] args) {
+        Student student = new Student("John Doe", 1, "A");
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(student, view);
+
+        controller.updateView();
+
+        controller.setStudentName("Jane Doe");
+        controller.setStudentGrade("B");
+        controller.updateView();
+    }
+}
