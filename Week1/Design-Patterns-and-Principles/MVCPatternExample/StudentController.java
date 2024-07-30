682a89ee669484d4adb2dfff8ee4da747f3bf6b9
Ankika Dey <ankikadey@outlook.com>
2024-07-30 11:35:28 +0530

    add controller class

---
+++ Week1/Design-Patterns-and-Principles/MVCPatternExample/StudentController.java
@@ -0,0 +1,37 @@
+public class StudentController {
+    private Student student;
+    private StudentView view;
+
+    public StudentController(Student student, StudentView view) {
+        this.student = student;
+        this.view = view;
+    }
+
+    public void setStudentName(String name) {
+        student.setName(name);
+    }
+
+    public String getStudentName() {
+        return student.getName();
+    }
+
+    public void setStudentId(int id) {
+        student.setId(id);
+    }
+
+    public int getStudentId() {
+        return student.getId();
+    }
+
+    public void setStudentGrade(String grade) {
+        student.setGrade(grade);
+    }
+
+    public String getStudentGrade() {
+        return student.getGrade();
+    }
+
+    public void updateView() {
+        view.displayStudentDetails(student);
+    }
+}
